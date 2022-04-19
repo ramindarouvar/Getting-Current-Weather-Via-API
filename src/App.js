d1182160b3009dda5579c4dbe3e414b0788e2a5b
Your Name <you@example.com>
2022-04-19 13:33:43 +0430

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { Fragment } from 'react';
 import Weather from './components/weather/Weather';
 
 function App() {
   return (
-    <div className="row justify-content-around py-5">
-      <div className="col-10">
-        <Weather />
+    <Fragment>
+      <div className="row justify-content-around py-5">
+        <div className="col-10">
+          <Weather />
+        </div>
       </div>
-    </div>
+      <footer className="fixed-bottom">
+            <div className="footer-navs">
+                <div className="container-md text-center">
+                    <div className="footer-copyright py-3">
+                    © کلیه حقوق این سامانه متعلق به رامین داروور  می‌باشد.
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </Fragment>
   );
 }
 
