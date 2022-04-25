450fdc4981ee6050b970a59e7520d04863d484d9
Your Name <you@example.com>
2022-04-25 19:33:33 +0430

    api key

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -3,10 +3,12 @@ import config from "./config.json";
 import axios from 'axios';
 
 export const getTheCityWeather = city => {
+    const apiKey = '30a23f064956499aafa152614221804';
     return axios.get(`${config.weatherapi}`, {
         params:{
+            key: apiKey,
             q: city,
             aqi: 'no'
-        }
+        },
     })
 }
